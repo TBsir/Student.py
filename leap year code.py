8f731f7854d2b43306a86d458c4e3d9ab955ef7c
TBsir <tooni4540@gmail.com>
2026-09-15 11:00:16 +0100

    Add comment on leap year calculation
    Added a comment explaining leap year logic.

---
+++ leap year code.py
@@ -0,0 +1,6 @@
+Year=int(input("Enter any year"))
+if Year%4==0:
+    print("This is a leap year")
+else:
+    print("This is not a leap year")
+  # Leap year happens every four years so 2024 divided by 4 is a leap year 2025 divide4 by 4 is not a leap year  
