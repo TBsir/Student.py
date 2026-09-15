eb5f9ef7a7fc550bbda34998774c5805986d5b3e
TBsir <tooni4540@gmail.com>
2026-09-15 11:00:52 +0100

    Add incomplete prime number checker script

---
+++ incomple prime number checker.py
@@ -0,0 +1,5 @@
+num=int(input("enter a number"))
+if num % num == 0 and num % 1==0:
+    print("This is a prime number")
+elif num not % num==0 and num not % 1==0:
+    print("This is an even number")
