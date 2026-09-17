e7008e3d4b3d27918dc2c28ec76a06cf365c6934
TBsir <tooni4540@gmail.com>
2026-09-17 14:28:32 +0100

    Add script to find the largest of three numbers

---
+++ Largest number.py
@@ -0,0 +1,9 @@
+num1=int(input("Enter a number"))
+num2=int(input("Enter a number"))
+num3=int(input("Enter a number"))
+if num1 >=num2 and num1>=num3:
+    print(f"The largest number is {num1}")
+elif num2>=num3 and num2>=num3:
+    print(f"The largest number is {num2}")
+else:
+    print(f"The largest number is {num3}")
